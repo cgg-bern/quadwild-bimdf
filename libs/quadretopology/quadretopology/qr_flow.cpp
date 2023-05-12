45753be4e471f1fbe616aa4dad0101f1580aa621
Martin Heistermann <git@mheistermann.de>
2023-05-12 13:25:52 +0200

    Add copyright header to qr_flow / link to paper

--- libs/quadretopology/quadretopology/qr_flow.cpp
+++ libs/quadretopology/quadretopology/qr_flow.cpp
@@ -1,3 +1,27 @@
+/***************************************************************************/
+/* Copyright(C) 2023 Martin Heistermann <martin.heistermann@unibe.ch>
+
+Implementation described in
+Min-Deviation-Flow in Bi-directed Graphs for T-Mesh Quantization, Martin Heistermann, Jethro Warnett, David Bommes, ACM Trans. Graph. 2023
+
+https://www.algohex.eu/publications/bimdf-quantization/
+
+
+All rights reserved.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+****************************************************************************/
+
 #include "qr_flow.h"
 #include "qr_flow_config.h"
 #include "qr_singularity_pairs.h"
